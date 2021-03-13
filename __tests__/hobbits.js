640089dc4cb43ac6a251c8c54c3e01a892169d29
Vladimir Turcan <vladimir.turcan@sumup.com>
2021-03-13 12:33:55 +0200

    finalize lecture

--- __tests__/hobbits.js
+++ __tests__/hobbits.js
@@ -1,14 +1,23 @@
 const supertest = require('supertest');
 const server = require('../server');
+const db = require('../data/config');
 
 describe('hobbits integration test', () => {
+  beforeEach(async () => {
+    await db.seed.run();
+  });
+
+  afterAll(async () => {
+    await db.destroy();
+  });
+
   test('gets a list of endpoints', async () => {
     const res = await supertest(server).get('/hobbits');
 
     // status code
     expect(res.status).toBe(200);
     // sa primesc json
-    expect(res.type).toBe('application/json')
+    expect(res.type).toBe('application/json');
     // ce date sa primesc
     expect(res.body.length).toBe(5);
     expect(res.body[0].id).toBe(1);
@@ -18,4 +27,17 @@ describe('hobbits integration test', () => {
   // de implementat hobbits/:id
   // atunci cand faci request la hobbit-ul cu id-ul 1 sa aveti un hobbit valid
   // /hobbits/50 sa primit un 404
+
+  test('creates a new hobbit', async () => {
+    const res = await supertest(server)
+      .post('/hobbits')
+      .send({ name: 'bilbo' });
+
+    expect(res.status).toBe(201);
+    expect(res.type).toBe('application/json');
+    expect(res.body.name).toBe('bilbo');
+
+    const getResponse = await supertest(server).get('/hobbits');
+    expect(getResponse.body).toHaveLength(6);
+  });
 });
--- hobbits/hobbits-router.js
+++ hobbits/hobbits-router.js
@@ -1,14 +1,24 @@
-const express = require("express")
-const Hobbits = require("./hobbits-model")
+const express = require('express');
+const Hobbits = require('./hobbits-model');
 
-const router = express.Router()
+const router = express.Router();
 
-router.get("/", async (req, res, next) => {
-	try {
-		res.json(await Hobbits.find())
-	} catch(err) {
-		next(err)
-	}
-})
+router.get('/', async (req, res, next) => {
+  try {
+    res.json(await Hobbits.find());
+  } catch (err) {
+    next(err);
+  }
+});
 
-module.exports = router
+router.post('/', async (req, res, next) => {
+  try {
+    const newHobbit = await Hobbits.create(req.body);
+
+    res.status(201).json(newHobbit);
+  } catch (error) {
+    next(err);
+  }
+});
+
+module.exports = router;
